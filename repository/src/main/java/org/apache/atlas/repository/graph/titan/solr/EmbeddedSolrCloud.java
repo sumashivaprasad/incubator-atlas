99cbebf763a2ad686bd47acbb01199828b5627a2
Suma Shivaprasad <sumasai.shivaprasad@gmail.com>
2015-07-29 18:10:17 +0530

    Fixed partially for solr4

--- repository/src/main/java/org/apache/atlas/repository/graph/titan/solr/EmbeddedSolrCloud.java
+++ repository/src/main/java/org/apache/atlas/repository/graph/titan/solr/EmbeddedSolrCloud.java
@@ -3,13 +3,9 @@ package org.apache.atlas.repository.graph.titan.solr;
 
 import com.google.common.base.Joiner;
 import org.apache.commons.io.FileUtils;
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.cloud.MiniSolrCloudCluster;
 import org.apache.solr.cloud.ZkController;
-import org.apache.solr.core.CoreContainer;
 import org.apache.solr.servlet.SolrDispatchFilter;
-import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +30,7 @@ public class EmbeddedSolrCloud {
 
     private EmbeddedSolrCloud() {
         String userDir = System.getProperty("user.dir");
-//        System.setProperty("zkRun", solrZKAddress);
+//      System.setProperty("zkRun", solrZKAddress);
         System.setProperty("bootstrap_conf", "true");
 
         String defaultSolrHome = Joiner.on(File.separator).join(userDir, "target", "classes", "titan", "solr");
@@ -42,27 +38,27 @@ public class EmbeddedSolrCloud {
     }
 
     public void start() throws Exception {
-        server = new MiniSolrCloudCluster(1, "/solr", new File(solrHome), new File(solrHome, "solr.xml"), null, null);
-//        server = new MiniSolrCloudCluster(1, "/solr", new File(solrHome, "solr.xml"), null, null);
-//        init();
+        //server = new MiniSolrCloudCluster(1, "/solr", new File(solrHome), new File(solrHome, "solr.xml"), null, null);
+      server = new MiniSolrCloudCluster(1, "/solr", new File(solrHome, "solr.xml"), null, null);
+//      init();
     }
 
-//    public void init() throws Exception {
-//        uploadConfigDirToZk("vertex_index", Joiner.on(File.separator).join(solrHome, "vertex_index"));
-//        uploadConfigDirToZk("edge_index", Joiner.on(File.separator).join(solrHome, "edge_index"));
-//        uploadConfigDirToZk("fulltext_index", Joiner.on(File.separator).join(solrHome, "fulltext_index"));
-//    }
-//
-//    private ZkController getZkController() {
-//        SolrDispatchFilter dispatchFilter =
-//            (SolrDispatchFilter) server.getJettySolrRunners().get(0).getDispatchFilter().getFilter();
-//        return dispatchFilter.getCores().getZkController();
-//    }
-//
-//    protected void uploadConfigDirToZk(String coreName, String collectionConfigDir) throws Exception {
-//        ZkController zkController = getZkController();
-//        zkController.uploadConfigDir(new File(collectionConfigDir), coreName);
-//    }
+    public void init() throws Exception {
+        uploadConfigDirToZk("vertex_index", Joiner.on(File.separator).join(solrHome, "vertex_index"));
+        uploadConfigDirToZk("edge_index", Joiner.on(File.separator).join(solrHome, "edge_index"));
+        uploadConfigDirToZk("fulltext_index", Joiner.on(File.separator).join(solrHome, "fulltext_index"));
+    }
+
+    private ZkController getZkController() {
+        SolrDispatchFilter dispatchFilter =
+            (SolrDispatchFilter) server.getJettySolrRunners().get(0).getDispatchFilter().getFilter();
+        return dispatchFilter.getCores().getZkController();
+    }
+
+    protected void uploadConfigDirToZk(String coreName, String collectionConfigDir) throws Exception {
+        ZkController zkController = getZkController();
+        zkController.uploadConfigDir(new File(collectionConfigDir), coreName);
+    }
 
     public static EmbeddedSolrCloud get() {
         if(INSTANCE == null) {
@@ -83,9 +79,9 @@ public class EmbeddedSolrCloud {
             }
             FileUtils.deleteDirectory(new File(Joiner.on(File.separator).join(solrHome, "zoo_data")));
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.info("Could not stop solr cloud ", e);
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.info("Could not stop solr cloud ", e);
         }
     }
 
--- repository/src/main/java/org/apache/atlas/repository/graph/titan/solr/SolrCloudClient.java
+++ repository/src/main/java/org/apache/atlas/repository/graph/titan/solr/SolrCloudClient.java
@@ -1,15 +1,14 @@
 package org.apache.atlas.repository.graph.titan.solr;
 
+import com.google.common.base.Joiner;
+import org.apache.atlas.ApplicationProperties;
+import org.apache.atlas.AtlasException;
 import org.apache.atlas.repository.Constants;
-import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.CloudSolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrServer;
 import org.apache.solr.client.solrj.request.CollectionAdminRequest;
 import org.apache.solr.client.solrj.response.CollectionAdminResponse;
 import org.apache.solr.cloud.ZkController;
-import org.apache.solr.common.SolrException;
-import org.apache.solr.common.cloud.ZkConfigManager;
 import org.apache.solr.common.util.NamedList;
 import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
@@ -17,45 +16,49 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class CloudSolr  {
+public class SolrCloudClient {
 
-    private static final Logger LOG = LoggerFactory.getLogger(CloudSolr.class);
-//  private static final String CONFIGSET_NAME = "conf";
-
+    private static final Logger LOG = LoggerFactory.getLogger(SolrCloudClient.class);
     private String zkAddress;
-    private CloudSolrClient client;
+    private String solrHome;
+    private CloudSolrServer client;
+    private static final String SOLR_HOME = "solr.solr.home";
 
-    public CloudSolr(String solrZKAddress) {
-        client = new CloudSolrClient(solrZKAddress);
+    public SolrCloudClient(String solrZKAddress) {
+        client = new CloudSolrServer(solrZKAddress);
         zkAddress = solrZKAddress;
+
+    }
+
+    public void connect() {
         client.connect();
     }
 
     public void close() throws IOException {
-        client.close();
+        client.shutdown();
     }
 
     public void uploadConfig(String configSetName) {
-        Path path = FileSystems.getDefault().getPath("...", "...");
+        String userDir = System.getProperty("user.dir");
+        String defaultSolrHome = Joiner.on(File.separator).join(userDir, "target", "classes", "titan", "solr");
+        solrHome = System.getProperty(SOLR_HOME, defaultSolrHome);
+        File vertexIndexConf = new File(solrHome, "vertex_index");
         try {
-            //TODO - check if conf already exists ??
-            final ZkConfigManager zkConfigManager = new ZkConfigManager(client.getZkStateReader().getZkClient());
-            zkConfigManager.uploadConfigDir(path, configSetName);
-//            ZkController.uploadConfigDir(server.getZkStateReader().getZkClient(), confDir, configSetName);
+            ZkController.uploadConfigDir(client.getZkStateReader().getZkClient(), vertexIndexConf, configSetName);
         } catch (IOException e) {
-//            throw new SolrException.ErrorCode("Unable to upload configuration" , e);
-            e.printStackTrace();
+            throw new RuntimeException("Could not upload configuration to zookeeper ", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Could not upload configuration to zookeeper ", e);
+        } catch (KeeperException e) {
+            throw new RuntimeException("Could not upload configuration to zookeeper ", e);
         }
     }
 
-    public void addCollections(String configSetName) throws IOException, SolrServerException {
-
+    public void addCollections(String configSetName) throws IOException, SolrServerException, AtlasException {
         uploadConfig(configSetName);
 
         List<String> collectionsToAdd = new ArrayList<String>() {{
@@ -65,12 +68,10 @@ public class CloudSolr  {
         }};
 
         for (String collectionName : collectionsToAdd) {
-//            final CollectionAdminResponse collection = CollectionAdminRequest.createCollection(collectionName, 1, configSetName, server);
             final CollectionAdminRequest.Create adminRequest = new CollectionAdminRequest.Create();
+            adminRequest.setCollectionName(collectionName);
             adminRequest.setConfigName(configSetName);
-            adminRequest.setNumShards(1);
-            adminRequest.setReplicationFactor(1);
-            final CollectionAdminResponse adminResponse = adminRequest.process(client, collectionName);
+            final CollectionAdminResponse adminResponse = adminRequest.process(client);
 
             if (!adminResponse.isSuccess()) {
                 final NamedList<String> errorMessages = adminResponse.getErrorMessages();
