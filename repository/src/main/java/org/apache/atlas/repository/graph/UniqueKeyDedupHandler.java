6c687327bd58a4f746b718c043524f3e87efb4ac
Suma Shivaprasad <sumasai.shivaprasad@gmail.com>
2016-05-25 21:36:50 -0700

    fixed tests

--- repository/src/main/java/org/apache/atlas/repository/graph/UniqueKeyDedupHandler.java
+++ repository/src/main/java/org/apache/atlas/repository/graph/UniqueKeyDedupHandler.java
@@ -16,30 +16,27 @@ public class UniqueKeyDedupHandler implements DedupHandler<ClassType, IReference
     private static final GraphHelper graphHelper = GraphHelper.getInstance();
 
     private static final Logger LOG = LoggerFactory.getLogger(UniqueKeyDedupHandler.class);
-    private Vertex instanceVertex;
 
     @Override
     public boolean exists(final ClassType classType, final IReferenceableInstance instance) throws AtlasException {
+        return vertex(classType, instance) != null;
+    }
+
+    @Override
+    public Vertex vertex(final ClassType classType, final IReferenceableInstance instance) throws AtlasException {
+        Vertex result = null;
         LOG.debug("Checking if there is an instance with the same unique attributes for instance {}", instance.toShortString());
         for (AttributeInfo attributeInfo : classType.fieldMapping().fields.values()) {
             if (attributeInfo.isUnique) {
                 String propertyKey = graphHelper.getQualifiedFieldName(classType, attributeInfo.name);
                 try {
-                    instanceVertex = graphHelper.getVertexForProperty(propertyKey, instance.get(attributeInfo.name));
+                    result = graphHelper.getVertexForProperty(propertyKey, instance.get(attributeInfo.name));
                     LOG.debug("Found vertex by unique attribute : " + propertyKey + "=" + instance.get(attributeInfo.name));
                 } catch (EntityNotFoundException e) {
                     //Its ok if there is no entity with the same unique value
                 }
             }
         }
-        return instanceVertex != null;
-    }
-
-    @Override
-    public Vertex vertex(final ClassType classType, final IReferenceableInstance instance) throws AtlasException {
-        if (instanceVertex == null) {
-            exists(classType, instance);
-        }
-        return instanceVertex;
+        return result;
     }
 }
