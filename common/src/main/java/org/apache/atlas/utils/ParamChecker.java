9d0fc91792a2669a214f6647191307b61b180ec1
Suma Shivaprasad <sumasai.shivaprasad@gmail.com>
2016-05-31 12:44:46 -0700

    Added param checks

--- common/src/main/java/org/apache/atlas/utils/ParamChecker.java
+++ common/src/main/java/org/apache/atlas/utils/ParamChecker.java
@@ -169,7 +169,8 @@ public final class ParamChecker {
 
     public static void equals(short source, short target, String name) {
         if (source != target) {
-            throw new IllegalArgumentException(name + " should be equal, current values ( " + source + "," + target + ')');
+            throw new IllegalArgumentException(name + " should be equal,"
+                + " current values ( " + source + "," + target + ')');
         }
     }
 }
