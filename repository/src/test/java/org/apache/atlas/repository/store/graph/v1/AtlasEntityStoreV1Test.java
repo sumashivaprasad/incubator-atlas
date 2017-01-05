a714fb19b77f33f375482a66835ebb5f60dd5d97
Suma Shivaprasad <sumasai.shivaprasad@gmail.com>
2017-01-05 15:28:10 -0800

    Adding test

---
+++ repository/src/test/java/org/apache/atlas/repository/store/graph/v1/AtlasEntityStoreV1Test.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.atlas.repository.store.graph.v1;
+
+import org.apache.atlas.RepositoryMetadataModule;
+import org.apache.atlas.TestUtils;
+import org.apache.atlas.TestUtilsV2;
+import org.apache.atlas.model.instance.AtlasEntity;
+import org.apache.atlas.repository.graph.GraphBackedSearchIndexer;
+import org.apache.atlas.type.AtlasTypeRegistry;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Guice;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Guice(modules = RepositoryMetadataModule.class)
+public class AtlasEntityStoreV1Test {
+
+    @Inject
+    AtlasTypeRegistry typeRegistry;
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        new GraphBackedSearchIndexer(new AtlasTypeRegistry());
+        TestUtilsV2.defineDeptEmployeeTypes();
+    }
+
+
+
+    @Test
+    public void testCreate() throws Exception {
+        AtlasEntity hrDept = TestUtilsV2.createDeptEg1();
+
+        List<String> guids = .createEntities(hrDept);
+        Assert.assertNotNull(guids);
+        Assert.assertEquals(guids.size(), 5);
+        guid = guids.get(4);
+        Assert.assertNotNull(guid);
+    }
+}
